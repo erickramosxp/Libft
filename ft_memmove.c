48e29833d3ecf1b5f08f613e34ab8f808ce3acac
erickramosxp <erickramossilva@gmail.com>
2023-10-28 19:30:42 -0300

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,7 +14,6 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
 	char	*dest2;
 	char	*src2;
 
@@ -24,11 +23,12 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		return (NULL);
 	if (dest2 > src2)
 	{
-		i = n - 1;
-		while (i >= 0)
+		dest2 = dest2 + (n - 1);
+		src2 = src2 + (n - 1);
+		while (n)
 		{
-			dest2[i] = src2[i];
-			i--;
+			*dest2-- = *src2--;
+			n--;
 		}
 	}
 	else
