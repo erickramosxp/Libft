2cbcf395bbdf24ca28dbf0e2a6fbd21caeddfc32
erickramosxp <erickramossilva@gmail.com>
2023-10-29 07:03:24 -0300

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: erramos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 18:28:40 by erramos           #+#    #+#             */
-/*   Updated: 2023/10/26 17:18:11 by erramos          ###   ########.fr       */
+/*   Updated: 2023/10/29 07:02:53 by erramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,20 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	unsigned char	*a;
+	unsigned char	*b;
 
+	a = (unsigned char *)s1;
+	b = (unsigned char *)s2;
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while (i < n)
 	{
-		if (s1[i] < s2[i])
+		if (a[i] < b[i])
 			return (-1);
-		else if (s1[i] > s2[i])
+		else if (a[i] > b[i])
 			return (1);
+		else if (a[i] == '\0' || b[i] == '\0')
+			return (0);
 		i++;
 	}
 	return (0);
