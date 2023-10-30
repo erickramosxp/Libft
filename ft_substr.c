ed27e904e61219f51ae0920c0f5d69b3a269b291
erramos <erickramossilva@outlook.com>
2023-10-30 15:51:24 -0300

    update

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: erramos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 17:22:27 by erramos           #+#    #+#             */
-/*   Updated: 2023/10/30 15:43:56 by erramos          ###   ########.fr       */
+/*   Updated: 2023/10/30 15:49:46 by erramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	//unsigned int	i;
 	unsigned int	j;
 	char			*sub;
 
@@ -23,9 +22,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	sub = (char *)malloc((len + 1) * sizeof(char));
 	if (!sub)
 		return (NULL);
-//	i = 0;
-	/*while (i < start)
-		i++;*/
 	j = 0;
 	while (s[start] != '\0' && j < len &&  start <= ft_strlen(s))
 	{
