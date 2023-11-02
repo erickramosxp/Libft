c3d3eb91e4e5929c561e05099a44840425f4c501
erickramosxp <erickramossilva@gmail.com>
2023-11-02 14:52:12 -0300

    ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,139 +6,78 @@
 /*   By: erramos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 13:32:54 by erramos           #+#    #+#             */
-/*   Updated: 2023/11/01 18:51:45 by erramos          ###   ########.fr       */
+/*   Updated: 2023/11/02 14:30:57 by erramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
-static int	count(char *s, char c, int start)
+int	position(char const *s, char c)
 {
-	while (s[start] != '\0')
+	int	pos;
+
+	pos = 0;
+	while (s[pos] != '\0')
 	{
-		if (s[start] == c)
-			return (start);
-		start++;
+		if (s[pos] != c && (s[pos + 1] == c || s[pos + 1] == '\0'))
+			return (pos + 1);
+		pos++;
 	}
-	return (start);
+	return (pos);
 }
 
-static char	**alloc(char *s, char **mat, char c, int line)
+char	**allocate_space(const char *s, char c, char **new, int sep)
 {
-	int     contador;
-	int     ax;
+	int	aux;
 	int	i;
 
 	i = 0;
-	contador = 0;
-        ax = 0;
-        while (i <= line)
-        {
-                contador = count(s, c, ax);
-//		printf("\nPalavra %d tamanho %d\n", i, contador - ax);
-		mat[i] = (char *)malloc((contador - ax + 1) * sizeof(char));
-		if (!mat[i])
-			return (NULL);
-                while (s[contador] == c && s[contador + 1] != '\0')
-                        contador++;
-                ax = contador;
-//		printf("\ntamanho %d\n", ax);
-                i++;
-        }
-	return (mat);
-}
-
-static char	**fill_matriz(char *s, char **new, char c, int line)
-{
-	int	contador;
-	int	i;
-
-	i = 0;
-	while (i <= line)
+	aux = 0;
+	while (i < sep)
 	{
-		contador = 0;
-		contador = count(s, c, contador);
-		ft_memcpy(new[i], s, contador);
-		new[i][contador] = '\0';
-		while (s[contador] == c)
-			contador++;
-		s = s + contador;
+		while (*s == c)
+			s++;
+		aux = position(s, c);	
+		new[i] = ft_substr(s, 0, aux);
+		s = s + aux;
 		i++;
 	}
-	new[i] = (char *)malloc(sizeof(char));
-	new[i][0] = '\0';
+	new[i] = '\0';
 	return (new);
 }
 
-/*
-static void	ft_fillsplit(char *s, char **r, char c)
-{
-	int	i;
-	int	j;
-	int	k;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] != c)
-		{
-			r[j][k] = s[i];
-			k++;
-		}
-		if (s[i] == c || s[i + 1] == '\0')
-		{
-			r[j][k] = '\0';
-			k = 0;
-			j++;
-			while (s[i + 1] == c && s[i + 1] != '\0')
-				i++;
-		}
-		i++;
-	}
-}
-*/
 char	**ft_split(char const *s, char c)
 {
-	int	i;
-	int	line;
 	char	**new;
 	char	*aux;
+	int		i;
+	int		sep;
 
+	sep = 0;
 	i = 0;
-	line = 0;
-	aux = ft_strtrim(s, &c);
-//	printf("%s", aux);
-	while (aux[i] != '\0')
+	aux = (char *)s;
+	while (s[i] != '\0')
 	{
-		if (aux[i] != c && aux[i + 1] == c)
-			line++;
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			sep++;
 		i++;
 	}
-	new = (char **)malloc((line + 2) * sizeof(char *));
-	alloc(aux, new, c, line);
-	fill_matriz(aux, new, c, line);
-//	ft_fillsplit(aux, new, c);
-//	printf("%d", line);
-	free(aux);	
+	new = malloc((sep + 1) * sizeof(char *));
+	if (!new)
+		return (NULL);
+	allocate_space(aux, c, new, sep);
 	return (new);
 }
 /*
 int	main(void)
 {
-	const char	*a = "     split  this for   me  !  a    ";
+	const char	*a = "      split       this for   me  !   a ";
 	char	**b;
 
 	b = ft_split(a, ' ');
 	
 	int i = 0;
-	while (b[i][0] != '\0')
-	{
-		printf("Palavra %d: %s\n", i, b[i]);
-		i++;
-	}
-	printf("%s", b[6]);
+       // printf("Palavra %d: %s\n", i, b[0]);
 //   	printf("Nulo :%s \n", b[5]);
 	return (0);
 }*/
