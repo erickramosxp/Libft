5e0033789eacbc2aaf600aeb09f90a0eb46a7bcc
erickramosxp <erickramossilva@gmail.com>
2023-11-02 14:44:59 -0300

    Update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: erramos <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 14:10:42 by erramos           #+#    #+#             */
-/*   Updated: 2023/10/29 16:53:31 by erramos          ###   ########.fr       */
+/*   Updated: 2023/11/02 14:42:28 by erramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (little[0] == '\0')
+	if (little[0] == '\0' || len == 0)
 		return ((char *)big);
 	while (big[i] != '\0' && i < len)
 	{
